b26027b7e13155df5c7ba6a7bc74000d131a2665
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-06 17:45:17 -0600

    building bast sturcture/test info

---
+++ Main/db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO /* column (var1, var2, var3 ect) */
+VALUES /*   (var1, var2, var3),
+            (var1, var2, var3),
+            (var1, var2, var3),
+            (var1, var2, var3),
+            (var1, var2, var3),
+            (var1, var2, var3);
+*/
+
+example:
+
+INSERT INTO names (first_name, last_name)
+Values  ("Jeremy", "Johnson"),
+        ("Kaela", "Johnson");
