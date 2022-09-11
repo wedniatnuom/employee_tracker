72bddad317630a0d82a662cc6255e7ceda6ac1a2
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-11 16:12:34 -0600

    table creation and seeding now works. Fixed syntax errors in the two .sql files

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,30 +1,30 @@
-DROP DATABSE IF EXISTS department_db;
-CREATE DATABSE department_db;
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
 
-USE department_db;
+USE department_db
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    dpt_name VARCHAR(30) NOT NULL,
+    dpt_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary INT DECIMAL,  /* NEED TO CHECK */
     dpt_id INT,
     FOREIGN KEY (dpt_id)
-    REFERENCES department(id)
+    REFERENCES departments(id),
+    salary DECIMAL(10 , 2)
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     FOREIGN KEY (role_id)
     REFERENCES roles(id),
     manager_id INT,
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id),
-)
+    REFERENCES employee(id)
+);
--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,20 +1,20 @@
-INSERT INTO department (id, dpt_name),
+INSERT INTO departments (id, dpt_name)
 VALUES  (1, "Engineering"),
         (2, "Finance"),
         (3, "Legal"),
         (4, "Sales");
 
-INSERT INTO roles (id, title, salary, dpt_id),
-VALUES  (1, "Sales Lead", "Sales", 100000),
-        (2, "Salesperson", "Sales", 80000),
-        (3, "Lead Engineer", "Engineering", 150000),
-        (4, "Software Engineer", "Engineering", 120000),
-        (5, "Account Manager", "Finance", 160000),
-        (6, "Accountant", "Finance", 125000),
-        (7, "Legal Team Lead", "Legal", 250000),
-        (8, "Lawyer", "Legal", 190000);
+INSERT INTO roles (id, title, dpt_id, salary)
+VALUES  (1, "Sales Lead", 4, 100000.00),
+        (2, "Salesperson", 4, 80000.00),
+        (3, "Lead Engineer", 1, 150000.00),
+        (4, "Software Engineer", 1, 120000.00),
+        (5, "Account Manager", 2, 160000.00),
+        (6, "Accountant", 2, 125000.00),
+        (7, "Legal Team Lead", 3, 250000.00),
+        (8, "Lawyer", 3, 190000.00);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id),
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES  (1, "John", "Doe", 1, NULL),
         (2, "Mike", "Chan", 2, 1),
         (3, "Ashley", "Rodriguez", 3, NULL),
